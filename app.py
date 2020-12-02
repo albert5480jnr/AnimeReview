7629c81f72fe8f6da84730967d34ed20f9d3340e
albert5480jnr <aj_sam@hotmail.co.uk>
2020-12-02 21:46:06 +0000

    web pages built

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bsom.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
     app= Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo= PyMongo(app)
+
     @app.route("/")
-    def hello():
-        return "Hello world"
+    @app.route("/Anime_list")
+    def Anime_list():
+        Anime = mongo.db.Anime.find()
+        return render_template("AnimeList.html", Anime=Anime)
 
 
 
