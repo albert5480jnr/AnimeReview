93aa908d55e233f1279e910aaf4cf193c0a5a623
albert5480jnr <aj_sam@hotmail.co.uk>
2020-12-07 19:01:54 +0000

    Navbar

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from bsom.objectid import ObjectId
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -14,17 +14,16 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo= PyMongo(app)
+mongo = PyMongo(app)
 
-    @app.route("/")
-    @app.route("/Anime_list")
-    def Anime_list():
-        Anime = mongo.db.Anime.find()
-        return render_template("AnimeList.html", Anime=Anime)
+@app.route("/")
+@app.route("/Anime_list ")
+def get_Anime_list():
+    Anime = mongo.db.tasks.find()
+    return render_template("AnimeList.html", Anime=Anime)
 
 
-
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
+    if __name__ == "__main__":
+        app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
