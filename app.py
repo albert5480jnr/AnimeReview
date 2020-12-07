7c1cab45f97bacf458e688d041a5530b759c6d4f
albert5480jnr <aj_sam@hotmail.co.uk>
2020-12-07 19:55:01 +0000

    register page

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if os.path.exists("env.py"):
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-
+from werkzeug.security import generate_password_hash, check_password_hash
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -21,6 +21,10 @@ mongo = PyMongo(app)
 def get_Anime_list():
     Anime = mongo.db.tasks.find()
     return render_template("AnimeList.html", Anime=Anime)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
     if __name__ == "__main__":
